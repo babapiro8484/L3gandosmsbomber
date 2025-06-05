7d722c30d62dac69f5007bd6ecc7f8ffad52e11d
babapiro8484 <ytwozy7@gmail.com>
2025-06-05 22:46:50 +0300

    Update Zerowsms.py

--- Zerowsms.py
+++ Zerowsms.py
@@ -26,7 +26,7 @@ while 1:
     Sms: {}           {}by {}@zerowbabaa\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
-        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (RELAX)\n\n 2- SMS Gönder (DEHŞET)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
+        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (RELAX)\n\n 2- SMS Gönder (DEHŞET)\n\n 3-SİKTİR GİT \n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
         if menu == "":
             continue
         menu = int(menu) 
